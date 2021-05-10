687191bd6963ad205b2c14d84e56cbe49e5f37ff
Anvar Khakimov <anvarkhakimov@MacBook-Pro-Anvar.local>
2021-05-10 23:31:00 +0300

    Made changes for homepage

---
+++ pages/flex.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function FlexPage() {
+    return (
+        <>
+            <Head>
+                <title>First post</title>
+            </Head>
+            <body>
+                <div className="max-w-2xl max-auto">
+                    <div className=""></div>
+                </div>
+            </body>
+        </>
+    )
+}
