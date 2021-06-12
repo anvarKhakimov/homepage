bc0a52df1199ad21f17ca21b022b56e266f98e3a
Anvar Khakimov <anvar.khakimov1@gmail.com>
2021-06-13 02:10:15 +0300

    Add resume.js

---
+++ pages/resume.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Resume() {
+    return (
+        <>
+            <Head>
+                <title>Анвар Хакимов — Резюме</title>
+            </Head>
+            <body>
+                <div className="max-w-2xl max-auto">
+                    <div className=""></div>
+                </div>
+            </body>
+        </>
+    )
+}
